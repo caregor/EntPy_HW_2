1eccc18dc8fa71f585c955e6c4d0efcb29910d5d
caregor <caregor@gmail.com>
2023-07-07 09:48:14 +0500

    Task 2 is done.

--- task2.py
+++ task2.py
@@ -3,3 +3,24 @@
 Напишите программу, которая получает целое число и возвращает его шестнадцатеричное строковое представление.
 Функцию hex используйте для проверки своего результата.
 """
+
+
+def decimal_to_hexadecimal(decimal):
+    result = ""
+    if decimal == 0:
+        result = "0"
+    else:
+        hex_chars = "0123456789ABCDEF"
+        while decimal > 0:
+            remainder = decimal % 16
+            result = hex_chars[remainder] + result
+            decimal = decimal // 16
+    return '0x' + result
+
+
+number = int(input("Введите целое число: "))
+
+hexadecimal = decimal_to_hexadecimal(number)
+
+print("Шестнадцатеричное представление числа:", hexadecimal)
+print("Проверка: hex =", hex(number))
